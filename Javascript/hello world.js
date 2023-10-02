aec2e2f9e73348257941013d5c9810dea3ca2b32
Siva Sai Vijay <109446711+Msiva19@users.noreply.github.com>
2023-10-02 16:21:03 +0530

    Add files via upload

---
+++ Javascript/hello world.js
@@ -0,0 +1,4 @@
+/*This is a simple JavaScript program that prints the message "Hello World" to the console.
+It is a good example of how to use the `console` module to interact with the user.*/
+
+console.log("Hello World");
